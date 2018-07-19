341c25b216f99243c588aa6cd9f41891d4941949
alevyt <kakolh23@gmail.com>
2018-07-19 03:58:34 +0300

    scripts cleanup

--- script/script.js
+++ script/script.js
@@ -4,32 +4,30 @@ function isoverflown(element) {
 
 function fitText(selector){
     let element = document.querySelector(selector);
-    
-    if(isoverflown(element)) {
-        console.log('resizing');
-        let fontSize = parseFloat(window.getComputedStyle(element, null).getPropertyValue('font-size'));
-        console.log('font-size-', fontSize-- + 'px');
+
+    element.style.fontSize = "50px";
+    let fontSize = parseFloat(window.getComputedStyle(element, null).getPropertyValue('font-size'));
+    while (isoverflown(element)){
         element.style.fontSize = --fontSize + 'px';
-        fitText(selector);
-    } else {
-        // element.style.fontSize = "50px";
-        // fitText(selector)
     }
 }
 
 document.querySelector('input[type="range"').addEventListener('change', (event) => {
-
-    console.log('range change', event.target.value);
     let outputElement = document.querySelector('.output-text'),
         rangeInput = document.querySelector('.range-input');
     rangeInput.innerHTML = outputElement.style = "width:" + event.target.value + "%";
     fitText('.output-text');
-
 })
 
 document.querySelector('#inputText').addEventListener('input', (event) => {
-    console.log(event.target.value);
     let outputElement = document.querySelector('.output-text');
     outputElement.innerHTML = event.target.value;
     fitText('.output-text');
 })
+
+document.onreadystatechange = () => {
+    let rangeInput = document.querySelector('input[type="range"]'),
+        rangeInputLabel = document.querySelector('.range-input');
+
+        rangeInputLabel.innerHTML = "width:" + rangeInput.value + "%";
+}
